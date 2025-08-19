01df785ce7b623ca29471b1333b14e2ec3a58f4c
deadlockinside <mytempmail228@internet.ru>
2025-08-19 15:02:57 +0300

    Add BookRepository

---
+++ infrastructure/Store.Memory/BookRepository.cs
@@ -0,0 +1,14 @@
+﻿namespace Store.Memory
+{
+    public class BookRepository : IBookRepository
+    {
+        private readonly Book[] _books = new[]
+        {
+            new Book (1, "Мышление миллионера"),
+            new Book (2, "Мастер куннилингуса. Секреты оральных ласк"),
+            new Book (3, "Русский язык 5 класс")
+        };
+
+        public Book[] GetAllByTitle(string titlePart) => _books.Where(book => book.Title.Contains(titlePart)).ToArray();
+    }
+}
