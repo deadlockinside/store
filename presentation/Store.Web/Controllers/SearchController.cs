2d3b22b9ad17a73b5630b9547dc7444cdb76f41d
deadlockinside <mytempmail228@internet.ru>
2025-08-19 15:53:34 +0300

    Add search view

---
+++ presentation/Store.Web/Controllers/SearchController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Store.Web.Controllers
+{
+    public class SearchController : Controller
+    {
+        private readonly IBookRepository _bookRepository;
+
+        public SearchController(IBookRepository bookRepository)
+        {
+            _bookRepository = bookRepository;
+        }
+
+        public IActionResult Index(string query) 
+        {
+            var books = _bookRepository.GetAllByTitle(query);
+
+            return View(books);
+        }
+    }
+}
